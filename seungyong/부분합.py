1617d0a39bca111c6f2fd3760709ad353d387fbc
gkfkt <https://github.com/gkfkktrh153/OrderSystem>
2022-11-27 17:16:15 +0900

    1주차 끄읏

--- seungyong/부분합.py
+++ seungyong/부분합.py
@@ -1,30 +1,10 @@
-#n = int(input())
-n = int(input())
-# 1234 / 1 % 10,  12 % 10, 123 % 10, 1234 % 10 
-# 1234 % 
-answer = 0
+array = [
+    ("A", 2, 300000) ,
+    ("A", 1, 10000),
+    ("B", 5, 200000),
+    ("A",5,5000000),
+    ("B", 70, 10000),
+    ("B", 70, 5000)
+]
 
-for i in range(1, n):
-    sum = 0
-    c = i
-    divide = 1
-    count = -1 
-
-    while (c != 0): # 자리수 탐색
-        c = c // 10
-        count += 1
-
-    divide = 10 ** count
-
-
-    while divide > 0:
-        a =  i // divide % 10
-        sum += a
-        divide = divide // 10
-
-    if sum + i == n:
-        answer = i
-        break
-        
-print(answer)
-
+print(sorted(array, key = lambda x: (x[0],x[1],x[2])))
