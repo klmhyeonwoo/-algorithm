87dc420b1412d206be83eaf3339e486458c5124d
klmhyeonwooo <gentlemonster77@naver.com>
2023-02-09 22:43:11 +0900

    SW 마에스트로 대비 정렬 (#27)

--- hyeonwoo/SW 마에스트로/기본 파일 입출력/10992.py
+++ hyeonwoo/SW 마에스트로/기본 파일 입출력/10992.py
@@ -4,7 +4,7 @@ N = int(sys.stdin.readline())
 
 for i in range(1, N+1):
     for a in range(N, i, -1):
-        print("-", end="")
+        print(" ", end="")
     for b in range(0, i*2-1):
         if (i == N):
             print("*", end="")
