a41a90da3a933eb51b250ecff2a7b00547863468
klmhyeonwooo <gentlemonster77@naver.com>
2023-03-15 23:35:58 +0900

    수정 중

---
+++ hyeonwoo/SW 마에스트로/그래프/10451.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const [t, ...arr] = fs.readFileSync("nodeJS.txt").toString().trim().split(/\s/);
+
+let count = 0;
+
+const DFS = (nodes, v, visited) => {
+    visited[v] = true;
+    for (let i of nodes[v]) {
+        if (visited[i] === false) {
+            DFS(nodes, i, visited);
+        }
+    }
+};
+
+const Sort = (list) => {
+    list.sort((a, b) => {
+        if (a > b) {
+            return 1;
+        }
+        if (a < b) {
+            return -1;
+        }
+        if (a === b) {
+            return 0;
+        }
+    });
+    return list;
+}
+
+for (let i=0; i<t; i++) {
+    const N = parseInt(arr.shift());
+    const permutation = arr.splice(0, N).map(Number);
+    const visited = [];
+    const nodes = [];
+
+    for (let k=0; k<=N; k++) {
+        visited.push(false);
+        nodes.push([]);
+    }
+
+
+    for (let z=1; z<permutation.length; z+=2) {
+        const a = permutation[z];
+
+        nodes[z].push(a);
+        nodes[a].push(z);
+    }
+
+    console.log(nodes)
+
+    for (let e=0; e<=N; e++) {
+        Sort(nodes[e]);
+    }
+
+    for (let c=1; c<visited.length; c++) {
+        if (visited[c] === false) {
+            DFS(nodes, c, visited);
+            count += 1;
+        }
+    }
+
+    console.log(count);
+}
